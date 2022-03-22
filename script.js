dea348ef1cee89b23d0e4d1eaff9e5e1ad755bd3
Nikolay <nyk190@mail.ru>
2022-03-22 17:05:34 +0300

    Add function computerPlay
    computerPlay function randomly choose "rock", "paper" or "scissors"

---
+++ script.js
@@ -0,0 +1,18 @@
+function computerPlay() {
+    let computerChoice;
+    switch (Math.floor(Math.random() * 3)) {
+        case 0:
+            computerChoice = 'rock';
+            break;
+        case 1:
+            computerChoice = 'paper';
+            break;
+        case 2:
+            computerChoice = 'scissors';
+            break;
+        default:
+            computerPlay();
+            break;
+    }
+    return computerChoice;
+}
