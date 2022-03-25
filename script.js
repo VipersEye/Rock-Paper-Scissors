b52d701e05cf929c4b9311eb1c2a1a9953569e27
Nikolay <nyk190@mail.ru>
2022-03-26 00:01:02 +0300

    Add counter of scores
    Unite playGame and playRound functions, add changing of scores
    in index.html

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let btnsSelection = document.querySelectorAll('.btn');
+let selectionImg = document.querySelector('.user-selection');
+let roundNumber = 0;
+let userScore = 0;
+let computerScore = 0;
+let userScoreText = document.querySelector('.user-score');
+let computerScoreText = document.querySelector('.computer-score');
+
+btnsSelection.forEach( btn => {
+    btn.addEventListener('click', evt => {
+        let btnValue = evt.target.value;
+        let newSrc = `./img/hand-${btnValue}.svg`
+        selectionImg.setAttribute('src', newSrc)
+        playRound(btnValue);    
+    });
+});
+
+
+
 function computerPlay() {
     let computerChoice;
     let selectionImg = document.querySelector('.computer-selection');
@@ -21,70 +40,63 @@ function computerPlay() {
     return computerChoice;
 }
 
-function userPlay () {
-    let userChoice = prompt("Choose rock, paper or scissors").toLowerCase();
-    if (userChoice === 'rock' || userChoice === 'paper' || userChoice === 'scissors') {
-        return userChoice;
-    }
-    else {
-        alert('Wrong! You must choose rock, paper or scissors');
-        return userPlay();
-    }
-}
-
 function playRound (userChoice) {
     let computerChoice = computerPlay();
+    let roundWinner;
 
     switch (true) {
         case (userChoice === computerChoice):
-            console.log('no one');
-            return ('no one');
+            roundWinner = 'no one';
+            break;
 
         case (userChoice === 'rock' && computerChoice === 'scissors'):
         case (userChoice === 'scissors' && computerChoice === 'paper'):
         case (userChoice === 'paper' && computerChoice === 'rock'):
-            console.log('user');
-            return ('player');
+            roundWinner = 'user';
+            break;
 
         case (computerChoice === 'rock' && userChoice === 'scissors'): 
         case (computerChoice === 'scissors' && userChoice === 'paper'):
         case (computerChoice === 'paper' && userChoice === 'rock'):
-            console.log('computer');
-            return ('computer');
+            roundWinner = 'computer';
+            break;
     }
+
+    if (roundWinner === 'user') {
+        userScore++;
+        userScoreText.textContent = userScore;
+    }
+    if (roundWinner === 'computer') {
+        computerScore++;
+        computerScoreText.textContent = computerScore;
+    }
+    roundNumber++;
 }
 
-function playGame () {
-    let userScore = 0;
-    let computerScore = 0;
-    let i = 0;
-    while (userScore < 5 && computerScore < 5) {
-        console.log(roundWinner + ' is the ' + (i + 1) + ' round winner');
-        if (roundWinner === 'player') userScore++;
-        if (roundWinner === 'computer') computerScore++;
-        i++;
-    }
-    if (userScore > computerScore) {
-        console.log('User win the game!'  + ' ' + userScore + '-' + computerScore);
-    }
-    else if (userScore < computerScore) {
-        console.log(' Computer win the game!'  + ' ' + userScore + '-' + computerScore) ;
-    }
+// while (userScore < 5 && computerScore < 5) {
+        
+        // }
+        // if (userScore > computerScore) {
+        //     console.log('User win the game!'  + ' ' + userScore + '-' + computerScore);
+        // }
+        // else if (userScore < computerScore) {
+        //     console.log(' Computer win the game!'  + ' ' + userScore + '-' + computerScore) ;
+        // }
+    
+        // else if (userScore === computerScore) {
+        //     console.log('Your score are equal!' + ' ' + userScore + '-' + computerScore);
+        // }
 
-    else if (userScore === computerScore) {
-        console.log('Your score are equal!' + ' ' + userScore + '-' + computerScore);
-    }
-}
+        // console.log(roundWinner + ' is the ' + (roundNumber + 1) + ' round winner');
 
-
-let btnsSelection = document.querySelectorAll('.btn');
-let selectionImg = document.querySelector('.user-selection');
-
-btnsSelection.forEach( btn => {
-    btn.addEventListener('click', evt => {
-        let btnValue = evt.target.value;
-        let newSrc = `./img/hand-${btnValue}.svg`
-        selectionImg.setAttribute('src', newSrc)
-        playRound(btnValue);    
-    });
-});
+        
+// function userPlay () {
+//     let userChoice = prompt("Choose rock, paper or scissors").toLowerCase();
+//     if (userChoice === 'rock' || userChoice === 'paper' || userChoice === 'scissors') {
+//         return userChoice;
+//     }
+//     else {
+//         alert('Wrong! You must choose rock, paper or scissors');
+//         return userPlay();
+//     }
+// }
